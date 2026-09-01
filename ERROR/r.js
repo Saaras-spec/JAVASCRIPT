2f4e8e98c4dab932e766244ccd3c4a9ce4e59c87
{saaras} <{saarasbisht@gmail.com}>
2026-09-01 21:01:08 +0530

    r.js

---
+++ ERROR/r.js
@@ -0,0 +1,7 @@
+let myName = "Alice";
+
+// ERROR! We misspelled the variable. JavaScript doesn't know what 'myname' is!
+console.log(myname);
+
+// ERROR! 'score' doesn't exist yet.
+console.log(score);
